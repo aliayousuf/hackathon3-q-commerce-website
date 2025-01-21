1cfc0611892f579b0bc4fc073861904902cd9b5b
alia <aliayousuf.izzat@gmail.com>
2025-01-22 04:17:03 +0500

    Add

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/stripe/error"
+      successUrl="https://hackathon3-q-commerce-website.vercel.app/stripe/success"
+      cancelUrl="https://hackathon3-q-commerce-website.vercel.app/stripe/error"
       currency="USD"
       billingAddressCollection={true}
       shouldPersist={true}
